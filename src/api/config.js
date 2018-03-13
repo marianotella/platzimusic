542d13915272bba0dba797702dda4d78229fd97d
Mariano Tellaeche <mtellaeche@inversorglobal.com.ar>
2018-03-13 15:18:36 -0300

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,9 @@
+export default {
+	appname: 'platzimusic',
+	apiKey: '00d9e03d456cea1813e8c46d2641127a',
+	secret:	'748a4babc47b066b04ab26e5a4a1e461',
+    registeredTo: 'marianotella'
+}
+
+
+
---
+++ src/components/artist.vue
@@ -0,0 +1,19 @@
+<template>
+	<li>
+		<h2><a :href="artist.url" target="_blank">{{ artist.name }}</a></h2>
+		<img :src="artist.image[2]['#text']">
+	</li>
+</template>
+
+<script>
+	export default {
+  		name: 'artist',
+  		props: ['artist']
+	}
+</script>
+
+<style type="text/css" scoped>
+	li{
+		margin: 10px;
+	}
+</style>
